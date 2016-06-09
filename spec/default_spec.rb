95a640424ab834a8a35e106acdadb713d51ae24e
Aaron Kalin <akalin@martinisoftware.com>
2016-06-08 22:47:19 -0500

    Adjust this test to a new reality of no fog

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -4,16 +4,6 @@ describe "dnsimple::default" do
   let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
 
   it "installs fog chef_gem" do
-    expect(chef_run).to install_chef_gem "fog"
-  end
-
-  context "when on the debian platform family" do
-    let(:chef_run) do
-      ChefSpec::SoloRunner.new(platform: "ubuntu").converge(described_recipe)
-    end
-
-    it "installs zlib1g-dev package" do
-      expect(chef_run).to install_package "zlib1g-dev"
-    end
+    expect(chef_run).to install_chef_gem "dnsimple"
   end
 end
